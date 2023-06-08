439eee3450114f1169fd2a36c8a3e58ff63a32d3
greencloud_us_jiakai <gujiakai28@gmail.com>
2023-06-08 14:23:52 +0800

    feat: fix docker compose port && add umami track code

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ export default function Document() {
 			<Head>
 				<meta name="description" content={process.env.NEXT_PUBLIC_SITE_DESCRIPTION} />
 				<meta name="keywords" content={process.env.NEXT_PUBLIC_SITE_KEYWORDS} />
+				<script async src="https://umami.gujiakai.top/script.js" data-website-id="89984862-f5d1-4d17-9cb1-54c121ea604d"></script>
 			</Head>
 			<body>
 				<Main />
