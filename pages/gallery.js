94662fd81d98f2df59e54de7b24e5ddcfc81bffd
jiakai <gujiakai28@gmail.com>
2023-06-08 21:12:02 +0800

    :bug: change react image gallery to yet-another-react-lightbox

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,15 +1,47 @@
-import Gallery from 'components/Gallery'
+import Layout from 'components/Layout'
+import { useState } from 'react'
+import Head from 'next/head'
 import getPhotoList from 'lib/getPhotoList'
+import getThumbnailList from 'lib/getThumbnailList'
+import PhotoAlbum from 'react-photo-album'
+import Lightbox from 'yet-another-react-lightbox'
+import Zoom from 'yet-another-react-lightbox/plugins/zoom'
+import 'yet-another-react-lightbox/styles.css'
+import useTranslation from 'next-translate/useTranslation'
+
+export default function GalleryPage({ images, thumbnails }) {
+	const [index, setIndex] = useState(-1)
+	const { t } = useTranslation('gallery')
+	const gallery = t('Gallery')
+	return (
+		<Layout>
+			<Head>
+				<title>{gallery}</title>
+			</Head>
+			<PhotoAlbum
+				layout="rows"
+				photos={thumbnails}
+				targetRowHeight={150}
+				onClick={({ index }) => setIndex(index)}
+			/>
+			 <Lightbox
+				open={index >= 0}
+				index={index}
+				close={() => setIndex(-1)}
+				slides={images}
+				plugins={[Zoom]}
+			/>
+		</Layout>
+	)
+}
 
 export async function getServerSideProps() {
 	const images = getPhotoList()
+	const thumbnails = getThumbnailList()
 	return {
 		props: {
 			images,
+			thumbnails,
 		},
 	}
-}
-
-export default function GalleryPage({ images }) {
-	return <Gallery images={images} className="z-10"/>
 }
