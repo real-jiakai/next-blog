3dad7279278d4125d4920fde93d1dbf1a3a96004
jiakai <gujiakai28@gmail.com>
2024-02-29 08:29:37 +0800

    fix: fix date

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
 	return (
 		<div className="flex justify-center items-center my-3 space-x-3">
 			<span className="text-md text-center font-medium">
-				{process.env.NEXT_PUBLIC_FOOTER}
+			© 2022-{new Date().getFullYear()} {process.env.NEXT_PUBLIC_FOOTER}
 			</span>
 			<span className="mx-2 text-gray-400">|</span>
 			<a href={process.env.NEXT_PUBLIC_GITHUB_REPO} target="_blank">
